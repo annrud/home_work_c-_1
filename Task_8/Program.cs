3a8f9039e15407a003c3d0eb94fa4253f7e0522c
annrud <rudina_anna88@mail.ru>
2022-09-17 19:39:39 +0800

    Add task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Программа выдаёт чётные числа от 1 до введённого числа.");
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();    
+int number = int.Parse(input);
+
+for (int i = 2; i <= number; i += 2)
+{
+    Console.Write(i);
+    if (i + 1 < number)
+    {
+        Console.Write(", ");
+    }
+}
