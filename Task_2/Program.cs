3684f4e9c3fca363fbb31b9f14527577556daa5f
annrud <rudina_anna88@mail.ru>
2022-09-17 18:14:43 +0800

    Add task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Программа определяет max и min из двух чисел.");
+Console.Write("Введите первое число: ");
+string input1 = Console.ReadLine();    
+int number1 = int.Parse(input1);
+
+Console.Write("Введите второе число: ");
+string input2 = Console.ReadLine(); 
+int number2 = int.Parse(input2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max={number1} min={number2}");
+}
+else
+{
+    Console.WriteLine($"max={number2} min={number1}");
+}
